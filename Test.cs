0a9ac204689a51f6e3b1e01ce838dfc4fc1c44c0
Rama-8844 <ramakrishna.t8844@gmail.com>
2020-10-07 16:30:14 +0530

    added a class

---
+++ Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Test
+{
+	public Test()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public void add()
+    {
+        return "hello world";
+    }
+
+}
